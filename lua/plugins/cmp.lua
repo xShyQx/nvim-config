5f5e3d4176096da69e25a92e8b4e1ba4347c5288
xShyQx <bart.karolczak@gmail.com>
2024-01-05 11:37:57 +0100

    Added LSPs for Android development

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,32 +1,37 @@
 return {
-  'hrsh7th/nvim-cmp',
+  "hrsh7th/nvim-cmp",
   dependencies = {
-    'hrsh7th/cmp-nvim-lsp',
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
-    'rafamadriz/friendly-snippets'
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
   },
   config = function()
     local cmp = require("cmp")
     cmp.setup({
       snippet = {
         expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end
+          require("luasnip").lsp_expand(args.body)
+        end,
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
       },
       mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true })
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
       }),
       sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'buffer' }
-      })
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+      }, {
+        { name = "buffer" },
+      }),
     })
-    require('luasnip.loaders.from_vscode').lazy_load()
-  end
+    require("luasnip.loaders.from_vscode").lazy_load()
+  end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,53 +1,58 @@
 local servers = {
-  'lua_ls', -- Lua
-  'tsserver', -- JavaScript, TypeScript, React
-  'jdtls', -- Java
-  'clangd', -- C++
-  'html', -- HTML
-  'cssls', -- CSS
-  'gradle_ls', -- Gradle, Groovy 
+  "lua_ls",                -- Lua
+  "tsserver",              -- JavaScript, TypeScript, React
+  "jdtls",                 -- Java
+  "clangd",                -- C++
+  "html",                  -- HTML
+  "cssls",                 -- CSS
+  "gradle_ls",             -- Gradle, Groovy
+  "kotlin_language_server", -- Kotlin
+  "lemminx",               -- XML
+  "jsonls",                -- JSON
 }
 
 return {
   {
-    'williamboman/mason.nvim',
+    "williamboman/mason.nvim",
     config = function()
-      require('mason').setup()
-    end
+      require("mason").setup()
+    end,
   },
   {
-    'williamboman/mason-lspconfig.nvim',
+    "williamboman/mason-lspconfig.nvim",
     config = function()
-      require('mason-lspconfig').setup({
-        ensure_installed = servers
+      require("mason-lspconfig").setup({
+        ensure_installed = servers,
       })
-    end
+    end,
   },
   {
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require('lspconfig')
-      require('mason-lspconfig').setup_handlers({
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      require("mason-lspconfig").setup_handlers({
         function(server)
-          lspconfig[server].setup({})
+          lspconfig[server].setup({ capabilities = capabilities })
         end,
-        ['jdtls'] = function() end
+        ["jdtls"] = function() end,
       })
 
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
         callback = function(ev)
           local opts = { buffer = ev.buf }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
-        end
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+        end,
       })
-    end
+    end,
   },
   {
-    'mfussenegger/nvim-jdtls'
-  }
+    "mfussenegger/nvim-jdtls",
+  },
 }
